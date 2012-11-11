2a0b08cf590bfc298c7bbafe85aa59e3bf58f10f
Marty Spellerberg <marty@halfempty.com>
2012-11-11 12:18:17 -0800

    Remove google analytics from theme

--- header.php
+++ header.php
@@ -21,23 +21,6 @@
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
-	<?php if (!is_user_logged_in()) : ?>
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-34363041-1']);
-	  _gaq.push(['_setDomainName', 'rocotillo.com']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	</script>
-	<?php endif; ?>
-
 	<?php wp_head(); ?>
 	
 </head>
